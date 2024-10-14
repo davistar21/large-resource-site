acb6ee9ded07c54824169f455c0bc38dfbee765b
David <eyitayobembe@gmail.com>
2024-10-14 12:32:40 +0100

    Create function buttons at the sidebar

--- DEAD or injured/scripts/guess-page.js
+++ DEAD or injured/scripts/guess-page.js
@@ -5,6 +5,12 @@ const ComputerSecretCode = generateSecretCode()
 const guessInputElem = document.querySelector('.guess-input')
 const goButtonElem = document.querySelector('.go-button')
 
+// Function to toggle the visibility of the menu
+function toggleMenu() {
+    const menuItems = document.getElementById('menuItems');
+    menuItems.classList.toggle('active');
+}
+document.querySelector('.hamburger').addEventListener('click',()=>{toggleMenu()})
 
 // console.log(guessInformation)
 let storedGuessInformation = JSON.parse(localStorage.getItem('guessInformation', guessInformation))
