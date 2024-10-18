ae860203f32d8c8a109125dceded514109a8abef
David <eyitayobembe@gmail.com>
2024-10-18 10:32:00 +0100

    Add font files

--- DEAD or injured/scripts/guess-page.js
+++ DEAD or injured/scripts/guess-page.js
@@ -4,21 +4,37 @@ import generateSecretCode from "./utils/generateSecretCode.js";
 import moveToNext from "./utils/moveToNext.js";
 
 const ComputerSecretCode = generateSecretCode()
-const guessInputElem = document.querySelector('.guess-input')
+const guessInputElem = document.querySelectorAll('.guess-input')
 const goButtonElem = document.querySelector('.go-button')
 
 export let guessInformation;
 guessInformation = JSON.parse(localStorage.getItem('guessInformation', guessInformation))
 
 document.querySelector('.hamburger').addEventListener('click',()=>{toggleMenu()})
+
 document.getElementById('1').focus();
-document.querySelectorAll('.guess-input').forEach(element => {
+
+let checkSet = new Set;
+let playerGuess = '';
+
+guessInputElem.forEach(element => {
     element.addEventListener('input', (e)=>{
-        let numberId = Number(element.id)
-        numberId++
+        checkSet.add(e.target.value);
+        playerGuess += e.target.value;
+        let numberId = Number(element.id);
+        numberId++;
         moveToNext(e.target, numberId, 4)
     })
 })
 
 // console.log(guessInformation)
-goButtonElem.addEventListener('click', ()=>{console.log(guessInformation)})
+goButtonElem.addEventListener('click', ()=>{
+    document.getElementById('1').focus();
+    checkSet.clear();
+    guessInformation.playerGuess = Number(playerGuess);
+    guessInputElem.forEach(element => {
+        element.value = ''
+    })
+    console.log(guessInformation)})
+
+
