20551a1a67b6d5ee338c6bbf0fc9e5883ff3c791
David <eyitayobembe@gmail.com>
2025-04-04 21:20:27 +0100

    GPA Calv

--- GPA calculator/gpa.js
+++ GPA calculator/gpa.js
@@ -1,15 +1,36 @@
+import handleDarkMode from "../utils/handleDarkMode.js"
 import $ from '../utils/querySelector.js'
 const circle = document.getElementById('progressCircle');
 const dashoffset = 440 - (440 * 10) / 100; // 440 is full circumference
 circle.style.strokeDashoffset = dashoffset;
 
-
+handleDarkMode();
 const addButtonElem = $('.add-button');
 const scoreData = $('.score-data');
+$('.clear-all').addEventListener('click', clearAll)
 addButtonElem.addEventListener('click', () => {
   regenerate();
-  console.log(scoreData)
+  g();
+  $('.each', scoreData, true).forEach(form => {
+    form.addEventListener('change', (e) => {
+      // calculateGPA(e.currentTarget.elements)
+    })
+  })
 })
+g()
+function g () {
+  $('.each', scoreData, true).forEach(form => {
+    form.addEventListener('change', (e) => {
+      e.preventDefault();
+      console.log(form)
+      // calculateGPA(e.currentTarget.elements)
+    })
+  })
+  
+}
+
+
+
 let length = 0;
 function regenerate() {
   $('.each', scoreData,  true).forEach((each, index) => {
@@ -18,7 +39,7 @@ function regenerate() {
   });
   // length += $('span', scoreData, true).length;
   scoreData.innerHTML += `
-    <div class="each" id="each-${length}">
+    <form class="each" id="each-${length}">
       <span>${length}</span>
       <input type="text" placeholder="GEG311">
       <select name="" id="">
@@ -31,8 +52,10 @@ function regenerate() {
       </select>
       <input type="number">
       <i class="fa-solid fa-trash delete-button" data-object-id="${length}" id="delete-button-${length}"></i>
-    </div>
-  `
+    </form>
+  `;
+  
+  length = $('.each', scoreData, true).length;
   $(`#each-${length}`, scoreData).classList.add('slide-down');
   deleteOne();
 }
@@ -41,7 +64,6 @@ function deleteOne () {
   $('.delete-button', scoreData, true).forEach(button => {
     button.addEventListener('click', () => {
       const objectId = button.dataset.objectId;
-      console.log($(`#each-${objectId}`, scoreData))
       $(`#each-${objectId}`, scoreData).remove();
 
       $('.each', scoreData,  true).forEach((each, index) => {
@@ -50,7 +72,24 @@ function deleteOne () {
     })
   })
 }
-
+function clearAll() {
+  scoreData.innerHTML = `
+    <form class="each" id="each-1">
+      <span>1</span>
+      <input type="text" placeholder="GEG311" name="course-code">
+      <select name="grade" id="">
+        <option value="A">A</option>
+        <option value="B">B</option>
+        <option value="C">C</option>
+        <option value="D">D</option>
+        <option value="E">E</option>
+        <option value="F">F</option>
+      </select>
+      <input type="number" name="course-units">
+      <i class="fa-solid fa-trash delete-button" data-object-id="1" id="delete-button-1"></i>
+    </form>
+    `
+}
 
 const gradeMap = [
   {
@@ -78,11 +117,16 @@ const gradeMap = [
     gradePoint: 0
   },
 ]
-
-// function calculateGPA() {
-//   $('.each', scoreData, true).forEach(each => {
-//     console.log(each)
-//   })
-// }
+let totalUnits = 0;
+let totalGradePoints = 0;
+function calculateGPA(elems) {
+  gradeMap.forEach(map => {
+    if (map.grade == elems['grade'].value){
+      totalGradePoints += Number(map.gradePoint)
+    }
+  })
+  console.log(totalGradePoints)
+}
 
 // calculateGPA()
+
