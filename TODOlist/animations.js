5b4e71fc59f26cc1d52874313575d8bc486c3df6
David <eyitayobembe@gmail.com>
2025-04-20 22:41:55 +0100

    Animate...

--- TODOlist/animations.js
+++ TODOlist/animations.js
@@ -1,9 +1,12 @@
 const bR1 = document.querySelector('.ball-row')
 const bR2 = document.querySelector('.ball-row-2')
+const bR3 = document.querySelector('.ball-row-3')
 bR1.querySelectorAll('.ball').forEach((ball, index) => {
   ball.style.animationDelay = `${index/10}s`
 })
 bR2.querySelectorAll('.ball').forEach((ball, index) => {
   ball.style.animationDelay = `${index/10}s`
 })
-
+bR3.querySelectorAll('.ball').forEach((ball, index) => {
+  ball.style.animationDelay = `${index/10}s`
+})
